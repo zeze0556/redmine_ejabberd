f54fc73f41c71d2acafb5ac7b1a73c9f8184f9c4
--==RIX==-- <zeze0556@gmail.com>
2017-07-28 19:26:29 +0800

    修改脚本错误

--- docker/compile.sh
+++ docker/compile.sh
@@ -5,6 +5,6 @@ export LANG=zh_CN.UTF-8
 mix local.rebar --force
 mix local.hex --force
 mix deps.get
-sed -i 's/rand_bytes/strong_rand_bytes/g' deps/romeo/lib/romeo/stanza.ex
+sed -i 's/crypto.rand_bytes/crypto.strong_rand_bytes/g' deps/romeo/lib/romeo/stanza.ex
 mix deps.compile
 mix compile
