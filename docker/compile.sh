ee1d0167f49702cdfeae1cf56e6c5f618ea11210
--==RIX==-- <zeze0556@gmail.com>
2017-07-28 18:34:44 +0800

    添加romeo编译补丁;添加与redmine集成例子

--- docker/compile.sh
+++ docker/compile.sh
@@ -4,4 +4,7 @@ set -x
 export LANG=zh_CN.UTF-8
 mix local.rebar --force
 mix local.hex --force
-mix deps.get && mix compile
+mix deps.get
+sed -i 's/rand_bytes/strong_rand_bytes/g' deps/romeo/lib/romeo/stanza.ex
+mix deps.compile
+mix compile
--- docker/start.sh
+++ docker/start.sh
@@ -4,12 +4,12 @@ set -x
 
 sleep 5
 
-ping -c 1 build 2>/dev/null 1>/dev/null
+ping -c 1 chatbuild 2>/dev/null 1>/dev/null
 
 while [ $? == 0 ]; do
     echo "build is working sleep 5"
     sleep 5;
-    ping -c 1 build 2>/dev/null 1>/dev/null
+    ping -c 1 chatbuild 2>/dev/null 1>/dev/null
 done
 
 echo "build is over start server last=$?"
